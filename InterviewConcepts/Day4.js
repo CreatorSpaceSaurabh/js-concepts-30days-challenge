cf5623ee824f875ac3e494b4bd70708f15a4ee28
unknown <gokussjindia@gmail.com>
2023-12-08 00:43:26 +0530

    Javascript interview concepts : Day 4 - Function method calling

--- InterviewConcepts/Day4.js
+++ InterviewConcepts/Day4.js
@@ -1,2 +1,18 @@
 // ****Day4*****
-// Question 1 - What will be the output of this program and should turn this program to expected output
+// Question  - Execute the function without calling its name
+function showResult() {
+  console.log("Here executing function");
+}
+const methodCall = "showResult";
+
+//Note: showResult(); //not allowed
+
+// Solution 1 : Wrap functional calling inside square bracket to make call
+// eval("methodCall"); // deprecated and not recommanded for use
+global[methodCall] = showResult;
+global[methodCall]();
+
+// Solution 2 : Assign function to another variable and make call
+// const callFunction = this[methodCall];
+// // callFunction();
+// console.log("global2", callFunction);
--- app.js
+++ app.js
@@ -1,4 +1,6 @@
 //  ***Note : To run particular Day file, uncomment the require statement and run npm start***
 // require("./InterviewConcepts/Day1");
 // require("./InterviewConcepts/Day2");
-require("./InterviewConcepts/Day3");
+// require("./InterviewConcepts/Day3");
+require("./InterviewConcepts/Day4");
+// require("./InterviewConcepts/Day5");
