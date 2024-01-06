02ad92eb49bb09de6423be1f773e12921a6237cd
unknown <gokussjindia@gmail.com>
2024-01-06 21:58:27 +0530

    Function overloading

--- ImportantConcepts/MethodOverloading.js
+++ ImportantConcepts/MethodOverloading.js
@@ -1,2 +1,13 @@
 // *****Method Overloading JS******
 // create parent class with property(variable) and the methods
+class Car {
+  speed = 100;
+  constructor(make, model) {
+    this.make = make;
+    this.model = model;
+  }
+
+  startEngine() {
+    console.log("Starting engine for ..Car");
+  }
+}
