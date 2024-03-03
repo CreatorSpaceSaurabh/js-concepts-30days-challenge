26d79f1a2d80f4597c1d0122e6b9428413739d84
unknown <gokussjindia@gmail.com>
2024-03-04 00:15:41 +0530

    Javascript - Cache implementation using add, addAll and  put method

--- ImportantConcepts/CachesInJs.js
+++ ImportantConcepts/CachesInJs.js
@@ -1,11 +1,30 @@
 // *****Caches in Javascript******
 
 let isCacheSupported = "caches" in window;
-let cacheName = "userSettings";
-let url = "/api/get/usersettings"; // Request for /api/get/usersettings url is sent to server, once the data is received, the response will be cached
-// add method
-caches.open(cacheName).then((cache) => {
-  cache.add(url).then(() => {
-    console.log("Data cached ");
+if (isCacheSupported) {
+  // create caches using 3 methods - add, addAll, put
+  let cacheName = "userSettings";
+  let url = "/api/get/usersettings"; // Request for /api/get/usersettings url is sent to server, once the data is received, the response will be cached
+  // add method
+  caches.open(cacheName).then((cache) => {
+    cache.add(url).then(() => {
+      console.log("Data cached ");
+    });
   });
-});
+
+  // all method - addAll accepts an array of URL , and returns a promise when all the resources are cached.
+
+  let urls = ["/get/userSettings?userId=1", "/get/userDetails"];
+  caches.open(cacheName).then((cache) => {
+    cache.addAll(urls).then(() => {
+      console.log("Data cached ");
+    });
+  });
+
+  //   put method - put adds a key/value pairs to current Cache object.In put we need to manually fetch the request and set the value.
+  fetch(url).then((res) => {
+    return caches.open(cacheName).then((cache) => {
+      return cache.put(url, res);
+    });
+  });
+}
