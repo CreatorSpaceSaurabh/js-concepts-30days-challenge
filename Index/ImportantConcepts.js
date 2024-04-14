c1b263ba6a91fcab84cd1eeca78fb89232bb928b
unknown <gokussjindia@gmail.com>
2024-04-14 22:40:41 +0530

    Optimize Architecture - Segregated indexes as per categories

---
+++ Index/ImportantConcepts.js
@@ -0,0 +1,9 @@
+// ****Important concepts******
+// require("../ImportantConcepts/MethodOverriding");
+// require("../ImportantConcepts/MethodOverloading");
+// require("../ImportantConcepts/ArrayMethods");
+// require("../ImportantConcepts/CurringInJs");
+// require("../ImportantConcepts/CustomJoin");
+// require("../ImportantConcepts/DateConversion");
+// require("../ImportantConcepts/RandomColorGenerator");
+require("../ImportantConcepts/RandomIdGenerator");
---
+++ Index/LeetCode.js
@@ -0,0 +1,4 @@
+// ******Leetcode ************
+// require("../LeetCode/LeetCode1");
+// require("../LeetCode/LeetCode2");
+// require("../LeetCode/Leetcode3");
