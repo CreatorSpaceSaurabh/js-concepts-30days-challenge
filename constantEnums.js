8dd0267895dbbbd52ab48939433830563d6bfd19
unknown <gokussjindia@gmail.com>
2024-04-16 23:18:56 +0530

    Code optimizations - Constant enums implementation and data calling

---
+++ constantEnums.js
@@ -0,0 +1,24 @@
+module.exports = {
+  data: [
+    {
+      id: 1,
+      name: "Peter parker",
+      age: 20,
+    },
+    {
+      id: 2,
+      name: "Bruce Banner",
+      age: 25,
+    },
+    {
+      id: 3,
+      name: "Tony Stark",
+      age: 45,
+    },
+    {
+      id: 4,
+      name: "Chris hemsworth",
+      age: 50,
+    },
+  ],
+};
