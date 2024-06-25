ae5074537571c109505007ec649fafd58b42437e
unknown <gokussjindia@gmail.com>
2024-06-25 21:41:53 +0530

    Index of solved so far questions

--- Index/SolvedSoFar.js
+++ Index/SolvedSoFar.js
@@ -1,3 +1,7 @@
 // ******Solved so far ************
-require("../SolvedSoFar/day1");
+// require("../SolvedSoFar/day1");
 // require("../SolvedSoFar/day2");
+// require("../SolvedSoFar/day3");
+// require("../SolvedSoFar/day4");
+// require("../SolvedSoFar/day5");
+require("../SolvedSoFar/day6");
