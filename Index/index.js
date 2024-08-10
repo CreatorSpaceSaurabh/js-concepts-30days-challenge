3c857003070d3a1c7059a31bff2cc5218aa0c88a
unknown <gokussjindia@gmail.com>
2024-08-10 21:08:49 +0530

    Message Queues - RabbitMQ implemnetation

--- Index/index.js
+++ Index/index.js
@@ -3,6 +3,6 @@ require("./ImportantConcepts");
 require("./InterviewConpets");
 require("./LeetCode");
 require("./SolvedSoFar");
-
+require("./MessageQueues");
 // ****Multi tenant****
 // require("../MultiTenant/connection");
---
+++ MessageQueue/rabbitMq.js
@@ -0,0 +1,48 @@
+const amqp = require("amqplib");
+
+async function connect() {
+  const connection = await amqp.connect("amqp://localhost");
+  // ... remaining steps
+
+  const channel = await connection.createChannel();
+
+  // Producer
+  const result = await channel.assertQueue("jobs");
+  const msg = { number: 42 };
+  channel.sendToQueue("jobs", Buffer.from(JSON.stringify(msg)));
+  console.log(`Sent job: ${msg.number}`);
+
+  // Consumer
+  await channel.assertQueue("jobs");
+
+  channel.consume("jobs", (message) => {
+    console.log(`Received job: ${message.content.toString()}`);
+    channel.ack(message);
+  });
+}
+
+// Promise/Async API example
+(async () => {
+  const queue = "tasks";
+  const conn = await amqplib.connect("amqp://localhost");
+
+  const ch1 = await conn.createChannel();
+  await ch1.assertQueue(queue);
+
+  // Listener
+  ch1.consume(queue, (msg) => {
+    if (msg !== null) {
+      console.log("Received:", msg.content.toString());
+      ch1.ack(msg);
+    } else {
+      console.log("Consumer cancelled by server");
+    }
+  });
+
+  // Sender
+  const ch2 = await conn.createChannel();
+
+  setInterval(() => {
+    ch2.sendToQueue(queue, Buffer.from("something to do"));
+  }, 1000);
+})();
