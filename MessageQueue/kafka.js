45d347e1ea29c9609e2a94233ab9dc7ff93d9420
unknown <gokussjindia@gmail.com>
2024-08-13 21:54:35 +0530

    Implementation of Kafka instance with consumer

--- MessageQueue/kafka.js
+++ MessageQueue/kafka.js
@@ -5,17 +5,51 @@ const kafka = new Kafka({
   brokers: ["localhost:9092", "localhost:9092"],
 });
 
+// ==============Producer===================
+
 const producer = kafka.producer();
 
-export const run = async () => {
+const runProducer = async (res) => {
   await producer.connect();
 
   await producer.send({
     topic: "kafka_topic",
-    messages: [{ value: "Message that wants to send to consumer" }],
+    messages: [
+      {
+        value: JSON.stringify(res), //"Message that wants to send to consumer"
+      },
+    ],
   });
 
   console.log("Message sent successfully");
 };
 
-module.exports = { run };
+// ==============Consumer===================
+const consumer = kafka.consumer({ groupId: "kafka" });
+
+const runConsumer = async () => {
+  await consumer.connect();
+  await consumer.subscribe({ topic: "kaffka_topic", fromBeginning: true });
+  await consumer.run({
+    eachMessage: async ({ message }) => {
+      console.log("*****Arrived in consumer*****");
+      const obj = JSON.parse(message.value);
+      console.log(obj);
+      // this part to create our user file
+      try {
+        const newFile = new File({
+          userId: Math.floor(Math.random()),
+          username: obj.username,
+        });
+        const response = await newFile.save();
+        if (response) {
+          console.log("File created successfully");
+        }
+      } catch (error) {
+        console.log("Catch error:", error);
+      }
+    },
+  });
+};
+
+module.exports = { runProducer, runConsumer };
