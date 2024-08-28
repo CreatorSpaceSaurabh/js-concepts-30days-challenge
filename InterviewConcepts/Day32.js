35d676d0fe9bb522fa4e542dfc4cbb732e5f9fc9
unknown <gokussjindia@gmail.com>
2024-08-28 23:36:05 +0530

    Day 32 - Snippet updations

--- InterviewConcepts/Day32.js
+++ InterviewConcepts/Day32.js
@@ -16,19 +16,19 @@ const sum = (...nums) => {
 console.log("Approach1 Dataset 1 - ", sum(2, 3));
 console.log("Approach1 Dataset 2 - ", sum(2, 3, 4));
 console.log("Approach1 Dataset 3 - ", sum(1, 2, 3, 4));
-// console.log("Approach1 Dataset 4 - ", sum(10, 2, 30, 4));
+console.log("Approach1 Dataset 4 - ", sum(10, 2, 30, 4));
 console.log("Approach1 Dataset 5 - ", sum(1, 20, -3, 4, 5, -2));
 
 // Solution 2 - Using spread arguments
-// const sum2 = () => {
-//   console.log("Arguments - ", arguments.length);
-//   return [...arguments].reduce((a, n) => {
-//     return a + n;
-//   }, 0);
-// };
+const sum2 = () => {
+  console.log("Arguments - ", arguments.length);
+  return [...arguments].reduce((a, n) => {
+    return a + n;
+  }, 0);
+};
 
-// console.log("Approach2 Dataset 1 - ", sum2(2, 3));
-// console.log("Approach2 Dataset 2 - ", sum2(2, 3, 4));
-// console.log("Approach2 Dataset 3 - ", sum2(1, 2, 3, 4));
-// console.log("Approach2 Dataset 4 - ", sum2(10, 2, 30, 4));
-// console.log("Approach2 Dataset 5 - ", sum2(1, 20, -3, 4, 5, -2));
+console.log("Approach2 Dataset 1 - ", sum2(2, 3));
+console.log("Approach2 Dataset 2 - ", sum2(2, 3, 4));
+console.log("Approach2 Dataset 3 - ", sum2(1, 2, 3, 4));
+console.log("Approach2 Dataset 4 - ", sum2(10, 2, 30, 4));
+console.log("Approach2 Dataset 5 - ", sum2(1, 20, -3, 4, 5, -2));
